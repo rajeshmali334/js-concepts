90dd89248ccf34cf7c3a80e3808028815271d8af
RAJESH <108955671+rajeshmali334@users.noreply.github.com>
2024-11-23 09:45:26 +0000

    01_vaiable js add

---
+++ js_basic/01_variabals.js
@@ -0,0 +1,15 @@
+const empID=123456;
+let empEmail = "rajesh@poornima@gmail.com";
+var passWord = "123456";
+empCity = "Jaipur"
+let empState;
+//empID = 1;
+empEmail = "hello@123";
+passWord = "00213";
+empCity = "Kota";
+console.log(empID);
+console.table([empID, empEmail, passWord, empCity, empState]);
+/*
+Perfer not use to var
+Because isuue of bloack scope and function scope
+ */
