b93c835b381f54892fafe3ab5c278cd61419e3e5
RAJESH <108955671+rajeshmali334@users.noreply.github.com>
2024-11-26 06:20:28 +0000

    Conversion of data type

---
+++ js_basic/03_conversionOpration.js
@@ -0,0 +1,44 @@
+// let score = "33";
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let ValueInNumber = Number(score);
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
+// let a = "22abc";
+// let ValueInNumber1=Number(a);
+// console.log(ValueInNumber1);
+// let a = null;
+// let ValueInNumber1=Number(a);
+// console.log(ValueInNumber1);
+// let a = true;
+// let ValueInNumber1=Number(a);
+// console.log(ValueInNumber1);
+// let a = false;
+// let ValueInNumber1=Number(a);
+// console.log(ValueInNumber1);
+
+// "33" => 33;
+// "33abc" => NaN;
+// true =>1; false => 0;
+
+// let isLogin = 1;
+// let booleanLogin = Boolean(isLogin);
+// console.log(booleanLogin);
+// let isLogin = 0;
+// let booleanLogin = Boolean(isLogin);
+// console.log(booleanLogin);
+// let isLogin = "";
+// let booleanLogin = Boolean(isLogin);
+// console.log(booleanLogin);
+let isLogin = "rajesh";
+let booleanLogin = Boolean(isLogin);
+console.log(booleanLogin);
+
+// 1=> true; 0 => false;
+// ""=> false, "rajesh"=> true;
+
+let rollNumber= 99;
+let stringNumber = String(rollNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
