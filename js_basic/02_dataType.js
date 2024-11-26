76e2dcbf2bca33dfff779069f71e1f3eb783e8c7
RAJESH <108955671+rajeshmali334@users.noreply.github.com>
2024-11-26 05:24:43 +0000

    datatypes

---
+++ js_basic/02_dataType.js
@@ -0,0 +1,29 @@
+"Use strict" // treat all js code as newer version
+
+//alert(3+3); // we are using node js, not browesr 
+console.log("Hello");
+console.log(3+
+    6); // code read ability node good 
+// documetation be MDN and tc39.es
+let name = "rajesh"  // String
+let age = 32; // Number
+let isLogin = true; // Boolean 
+// Data type are two type primitive and non primitive
+
+/* Primitive Data type * /
+// number => range 2 power 53;
+// bigInt ;
+// String;
+// boolean => false / true ;
+// null => stanlone value;
+// undefined = > be the not declare data so auto define data is undefined data;
+// symbol => unique 
+ 
+/* non-Primitive Datatype */
+
+// Object
+
+//console.log(typeof null);
+//console.log(typeof undefined);
+
+// console.log(typeof age);
