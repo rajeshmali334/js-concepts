2679da1fb72a6811c2a35aca7f06ef5f32bced5d
RAJESH <108955671+rajeshmali334@users.noreply.github.com>
2024-11-26 07:13:40 +0000

    Opration

--- js_basic/03_conversionOpration.js
+++ js_basic/03_conversionOpration.js
@@ -33,12 +33,38 @@
 // console.log(booleanLogin);
 let isLogin = "rajesh";
 let booleanLogin = Boolean(isLogin);
-console.log(booleanLogin);
+//console.log(booleanLogin);
 
 // 1=> true; 0 => false;
 // ""=> false, "rajesh"=> true;
 
 let rollNumber= 99;
 let stringNumber = String(rollNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+/* -------- Opration ---------- */
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**4);
+// console.log(11%2);
+let str1 = "Rajesh";
+let Str2 = " Mali";
+let Str= str1+Str2;
+//console.log(Str);
+// console.log("1"+ 3 );
+// console.log("1"+2+2);
+// console.log(2+1+"3");
+// console.log((5+2)*3%2);
+// console.log(+true);
+// console.log(+"");
+
+// let count =100;
+// count++;
+// console.log(count); // 101
+let count =100;
+count++;
+console.log(count);
